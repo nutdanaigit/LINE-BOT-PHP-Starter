e653b4221be4fd3c5bb5ff9f4abe1e4ce193d29b
Nutdanai <Nutdanai.P@augmentis.biz>
2016-11-14 16:23:32 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -51,8 +51,8 @@ if (!is_null($events['events'])) {
 					'area' => $area,
 				];
 				$baseSize = [
-					'height' => 240,
-					'width' => 100,
+					'height' => 100,
+					'width' => 240,
 				];
 				// Build message to reply back
 				$messages = [
