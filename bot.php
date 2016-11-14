bb18280a09505304089c95ae4338a8f64af12502
Nutdanai <Nutdanai.P@augmentis.biz>
2016-11-14 18:00:25 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -9,7 +9,6 @@ $events = json_decode($content, true);
 $arrayWebInformation = array('1','web','Web','เว็บ','เว็บไซต์');
 $arrayPhone = array('2','May i have numberphone please?','phone','Phone','phone please','Phone Please','เบอร์','ขอเบอร์','ขอเบอร์หน่อย','ติดต่อพนักงาน','โทร','หมายเลข','เบอร์โทร','ขอเบอร์โทร','ขอเบอร์โทรครับ','ขอบเบอร์โทรค่ะ','เบอร์โทรครับ');
 $arrayInformation = array('Information','information','ขอข้อมูล','ข้อมูล','สอบถามข้อมูลเพิ่มเติม','รายละเอียด','#');
-$check = 'Click To WebSite';
 
 	function imageMap(){
 				
@@ -86,7 +85,7 @@ if (!is_null($events['events'])) {
 --> พิมพ์ 1 หรือ web หรือ เว็บ เพื่อเข้าดูข้อมูลที่เว็ปไซต์ค่ะ  
 --> พิมพ์ 2 หรือ phone หรือ เบอร์ เพื่อโทรออกค่ะ ',
 				];
-			}else if(in_array($event['message']['text'], $check )){
+			}else if(in_array($event['message']['text'],'Click To WebSite' )){
 				// Do in the future.
 			}else{
 				$messages = [
